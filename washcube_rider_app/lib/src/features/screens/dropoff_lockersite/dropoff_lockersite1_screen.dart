540dc84a41a17faedc300faed8b72dce2e26a9b5
YonWaiHung <waihung.yon@gmail.com>
2024-02-26 17:36:06 +0800

    merge conflict resolution

--- washcube_rider_app/lib/src/features/screens/dropoff_lockersite/dropoff_lockersite1_screen.dart
+++ washcube_rider_app/lib/src/features/screens/dropoff_lockersite/dropoff_lockersite1_screen.dart
@@ -4,10 +4,10 @@ import 'package:washcube_rider_app/src/common_widgets/help_widget.dart';
 import 'package:washcube_rider_app/src/constants/colors.dart';
 import 'package:washcube_rider_app/src/constants/image_strings.dart';
 import 'package:washcube_rider_app/src/constants/sizes.dart';
+import 'package:washcube_rider_app/src/features/screens/dropoff_lockersite/dropoff_lockersite2_screen.dart';
 import 'package:washcube_rider_app/src/features/screens/id_rider/id_verification_screen.dart';
 import 'package:washcube_rider_app/src/utilities/theme/widget_themes/text_theme.dart';
 import '../locker_qr/locker_qr_screen.dart';
-import 'dropoff_lockersite2_screen.dart';
 
 class DropOffLocker extends StatefulWidget {
   const DropOffLocker({super.key});
